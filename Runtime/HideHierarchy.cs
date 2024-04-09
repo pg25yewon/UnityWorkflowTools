06e585dca72d152e33f8940f5a9dcf0ba532234e
pg25yewon <pg25yewon@vfs.com>
2024-04-09 14:42:48 -0700

    added hierarchy hide script

---
+++ Runtime/HideHierarchy.cs
@@ -0,0 +1,42 @@
+// Copyright 2023 - 2024 Yewon Jung
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityWorkflowTools
+{
+    // if does both in edit mode and play mode 
+    [ExecuteAlways]
+    public class HideHierarchy : MonoBehaviour
+    {
+#if UNITY_EDITOR
+
+        [SerializeField] private bool _hidden = true;
+
+        private void OnValidate()
+        {
+            HideChildren(_hidden);
+        }
+
+        private void Awake()
+        {
+            HideChildren(_hidden);
+        }
+
+        private void HideChildren(bool hidden)
+        {
+            Transform[] children = GetComponentsInChildren<Transform>();
+
+            foreach (Transform child in children)
+            {
+                if (child.gameObject == gameObject) continue;
+
+                HideFlags flags = HideFlags.HideInHierarchy | HideFlags.HideInInspector;
+                if (!hidden) flags = HideFlags.None;
+                child.gameObject.hideFlags = flags;
+            }
+        }
+#endif
+    }
+}
